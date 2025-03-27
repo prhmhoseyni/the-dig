ea41c97e1480b02fb7d90fe73b33a0e815d8b094
prhmhoseyni <prhmhoseyni@gmail.com>
2025-03-27 08:42:09 +0330

    feat: initial config storybook project

---
+++ projects/storybook/src/stories/Button.ts
@@ -0,0 +1,34 @@
+import { html } from 'lit';
+import { styleMap } from 'lit/directives/style-map.js';
+
+import './button.css';
+
+export interface ButtonProps {
+  /** Is this the principal call to action on the page? */
+  primary?: boolean;
+  /** What background color to use */
+  backgroundColor?: string;
+  /** How large should the button be? */
+  size?: 'small' | 'medium' | 'large';
+  /** Button contents */
+  label: string;
+  /** Optional click handler */
+  onClick?: () => void;
+}
+/** Primary UI component for user interaction */
+export const Button = ({ primary, backgroundColor, size, label, onClick }: ButtonProps) => {
+  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+
+  return html`
+    <div class="text-brand">تست</div>
+    <div class="type">تست</div>
+    <button
+      type="button"
+      class=${['storybook-button', `storybook-button--${size || 'medium'}`, mode].join(' ')}
+      style=${styleMap({ backgroundColor })}
+      @click=${onClick}
+    >
+      ${label}
+    </button>
+  `;
+};
