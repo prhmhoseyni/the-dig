353f7516018399848fa654b68bc5db39255a8924
Parham Hoseyni <150419715+prhmhoseyni@users.noreply.github.com>
2025-08-01 14:44:47 +0330

    feat: implement radio group

---
+++ apps/docs/src/app/components/radio-group/page.tsx
@@ -0,0 +1,14 @@
+import RadioGroup from "@repo/ui/RadioGroup";
+
+export default function InputPage() {
+    return (
+        <>
+            <div className="flex gap-4 p-8">
+                <RadioGroup name="name" value="1" />
+                <RadioGroup name="name" value="2" />
+                <RadioGroup name="name" value="3" />
+            </div>
+
+        </>
+    )
+}
---
+++ packages/ui/src/RadioGroup/index.tsx
@@ -0,0 +1,20 @@
+import { type InputHTMLAttributes, type DetailedHTMLProps } from "react";
+import clsx from "clsx";
+import styles from "./index.module.css";
+
+
+
+export default function RadioGroup(props: DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>) {
+    const {
+        className = "",
+        ...rest
+    } = props;
+
+    return (
+        <input
+            type="radio"
+            className={clsx(styles["radio"], className)}
+            {...rest}
+        />
+    );
+}
