33e4054ac0c05df33e7184d4ea87764ccbe4f21b
Parham Hoseyni <150419715+prhmhoseyni@users.noreply.github.com>
2025-08-01 11:37:00 +0330

    feat: update chip

--- packages/ui/src/Chip/index.tsx
+++ packages/ui/src/Chip/index.tsx
@@ -54,7 +54,7 @@ export default function Chip(props: Props) {
 
             {children}
 
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none" className="relative inset-x-0.5">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none" className="relative inset-x-1">
                 <path
                     fillRule="evenodd"
                     clipRule="evenodd"
