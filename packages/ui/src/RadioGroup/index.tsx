c18ba7d419af80821ebdb377e0b74f1856fb8816
Parham Hoseyni <150419715+prhmhoseyni@users.noreply.github.com>
2025-08-01 14:46:43 +0330

    fix: fix bug style module import

--- packages/ui/src/RadioGroup/index.tsx
+++ packages/ui/src/RadioGroup/index.tsx
@@ -2,8 +2,6 @@ import { type InputHTMLAttributes, type DetailedHTMLProps } from "react";
 import clsx from "clsx";
 import styles from "./index.module.css";
 
-
-
 export default function RadioGroup(props: DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>) {
     const {
         className = "",
