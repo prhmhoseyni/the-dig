825a8802ad35ef55ba8e7d14314adb7e0e3fb11f
AmirHosseinRP <rezapanahhf1@gmail.com>
2025-08-03 07:59:06 +0330

    feat: size examples added

--- apps/docs/src/app/components/switch/page.tsx
+++ apps/docs/src/app/components/switch/page.tsx
@@ -2,13 +2,15 @@ import Switch from "@repo/ui/Switch";
 
 export default function SwitchPage() {
   return (
-    <div className="container mx-auto flex gap-3 p-4">
+    <div className="container mx-auto flex gap-3 p-4 justify-center">
       <Switch color="brand" />
       <Switch color="danger" />
       <Switch color="info" />
       <Switch color="gray" />
       <Switch color="success" />
       <Switch color="warning" />
+      <Switch size="lg" />
+      <Switch size="sm" />
       <Switch isLoading />
       <Switch disabled />
     </div>
