30ea2bb6d6291f348d00c863aeec7119598f43be
Parham Hoseyni <150419715+prhmhoseyni@users.noreply.github.com>
2025-08-12 07:56:13 +0330

    feat: update docs app layout

--- apps/docs/src/app/layout.tsx
+++ apps/docs/src/app/layout.tsx
@@ -23,8 +23,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>{children}</body>
+    <html lang="fa" dir="rtl">
+      <body className={`${geistSans.variable} ${geistMono.variable} bg-[#FFFFFF]`}>{children}</body>
     </html>
   );
 }
