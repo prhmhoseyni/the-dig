dcf3cbe34132c7dfcd79968668df73aa672cf7e7
Parham Hoseyni <150419715+prhmhoseyni@users.noreply.github.com>
2025-08-26 12:47:50 +0330

    feat: implement pagination

--- apps/docs/src/app/components/pagination/page.tsx
+++ apps/docs/src/app/components/pagination/page.tsx
@@ -1,3 +1,16 @@
+"use client";
+
+import { useState } from "react";
+import Pagination from "@repo/ui/Pagination";
+
 export default function PaginationPage() {
-  return <div>Pagination Page</div>;
+  const [page, setPage] = useState(3);
+
+  console.log(page);
+
+  return (
+    <div className="flex items-center gap-4 p-4">
+      <Pagination total={4} page={page} onChange={(_page) => setPage(_page)} />
+    </div>
+  );
 }
