039f49511044e8e9a5384ff8906673896a21ef48
Parham Hoseyni <150419715+prhmhoseyni@users.noreply.github.com>
2025-08-26 08:33:13 +0330

    feat: implement datepicker

---
+++ apps/docs/src/app/components/date-picker/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useState } from "react";
+import DatePicker from "@repo/ui/DatePicker";
+
+export default function Page() {
+  const [value, setValue] = useState<number | null>(null);
+
+  return (
+    <div className="flex items-center gap-4 p-4">
+      <DatePicker value={value} onChange={(val) => setValue(val)} inputProps={{ placeholder: "تاریخ را انتخاب کنید" }} />
+    </div>
+  );
+}
---
+++ packages/ui/src/DatePicker/index.tsx
@@ -0,0 +1,69 @@
+import { type InputHTMLAttributes, type RefObject } from "react";
+import ReactModernDatePicker, { DayValue } from "@hassanmojab/react-modern-calendar-datepicker";
+import { toEnglishDigits } from "msk-utils";
+import moment from "jalali-moment";
+import Input from "../Input";
+import "./datepicker.css";
+
+function convertTimestamp2DayValue(value: number) {
+  return {
+    day: Number(toEnglishDigits(new Date(value).toLocaleDateString("fa", { day: "numeric" }))),
+    month: Number(toEnglishDigits(new Date(value).toLocaleDateString("fa", { month: "numeric" }))),
+    year: Number(toEnglishDigits(new Date(value).toLocaleDateString("fa", { year: "numeric" }))),
+  };
+}
+
+function convertDayValue2Timestamp(value: DayValue) {
+  const convertedDateFromSolarToGregorian = moment
+    .from(`${value?.year}/${value?.month}/${value?.day}`, "fa", "YYYY/MM/DD")
+    .format("YYYY/MM/DD");
+
+  return new Date(convertedDateFromSolarToGregorian).getTime();
+}
+
+export interface DatePickerProps {
+  value: number | null;
+  onChange: (value: number | null) => void;
+  minDate?: number;
+  maxDate?: number;
+  inputProps?: InputHTMLAttributes<HTMLInputElement>;
+}
+
+export default function DatePicker(props: DatePickerProps) {
+  const render = ({ ref }: { ref: RefObject<HTMLElement> }) => (
+    <div className="relative w-full">
+      <Input
+        dir="rtl"
+        ref={ref as RefObject<HTMLInputElement>}
+        readOnly
+        value={
+          props.value
+            ? new Date(props.value).toLocaleDateString("fa", {
+                day: "numeric",
+                month: "long",
+                year: "numeric",
+              })
+            : undefined
+        }
+        {...props.inputProps}
+      />
+    </div>
+  );
+
+  return (
+    <ReactModernDatePicker
+      locale="fa"
+      value={props.value ? convertTimestamp2DayValue(props.value) : null}
+      shouldHighlightWeekends
+      renderInput={render}
+      maximumDate={props.maxDate ? convertTimestamp2DayValue(props.maxDate) : undefined}
+      minimumDate={props.minDate ? convertTimestamp2DayValue(props.minDate) : undefined}
+      colorPrimary="rgb(var(--dig-brand))"
+      onChange={(_day) => {
+        if (_day) {
+          props.onChange(convertDayValue2Timestamp(_day));
+        }
+      }}
+    />
+  );
+}
