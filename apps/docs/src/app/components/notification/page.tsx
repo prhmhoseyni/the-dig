c8c681cb64cb883fd02b1cf399c16e5855593181
Parham Hoseyni <150419715+prhmhoseyni@users.noreply.github.com>
2025-08-26 15:32:21 +0330

    feat: implement notification component

--- apps/docs/src/app/components/notification/page.tsx
+++ apps/docs/src/app/components/notification/page.tsx
@@ -2,26 +2,39 @@
 
 import Button from "@repo/ui/Button";
 import Notification from "@repo/ui/Notification";
+import { toast, Toaster } from "sonner";
 
 export default function NotificationPage() {
   return (
-    <div dir="rtl" className="bg-[#7e7e7e60] p-10">
-      <Notification
-        title="به‌روزرسانی جدید"
-        description="نسخه جدید آماده نصب است. برای بهبود عملکرد به‌روزرسانی کنید."
-        actions={
-          <div className="flex gap-3">
-            <Button color="gray" variant="outlined">
-              مشاهده تغییرات
-            </Button>
+    <div className="flex items-center gap-4 p-4">
+      <div>
+        <Toaster closeButton />
 
-            <Button color="gray" variant="outlined">
-              به‌روزرسانی
-            </Button>
-          </div>
-        }
-        onClose={() => null}
-      />
+        <button
+          onClick={() =>
+            toast(
+              <Notification
+                title="به‌روزرسانی جدید"
+                description="نسخه جدید آماده نصب است. برای بهبود عملکرد به‌روزرسانی کنید."
+                actions={
+                  <div className="flex gap-3">
+                    <Button color="gray" variant="outlined">
+                      مشاهده تغییرات
+                    </Button>
+
+                    <Button color="gray" variant="outlined">
+                      به‌روزرسانی
+                    </Button>
+                  </div>
+                }
+                onClose={() => null}
+              />,
+            )
+          }
+        >
+          Give me a toast
+        </button>
+      </div>
     </div>
   );
 }
