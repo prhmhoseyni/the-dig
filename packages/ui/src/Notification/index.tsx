dc06f9a049ae00fd5bfd48fd573ad78ae7d59413
Parham Hoseyni <150419715+prhmhoseyni@users.noreply.github.com>
2025-08-26 15:33:02 +0330

    feat: implement notification component

--- packages/ui/src/Notification/index.tsx
+++ packages/ui/src/Notification/index.tsx
@@ -1,5 +1,5 @@
 import clsx from "clsx";
-import { Info, X } from "lucide-react";
+import { Info } from "lucide-react";
 import { memo, type ReactNode } from "react";
 
 interface Props {
@@ -11,16 +11,11 @@ interface Props {
 }
 
 const Notification = (props: Props) => {
-  const { title, description, actions, onClose, className = "" } = props;
+  const { title, description, actions, className = "" } = props;
 
   return (
-    <div
-      className={clsx(
-        "bg-background-primary border border-gray-light-hover shadow-3xl p-4 rounded-xl flex flex-row justify-between items-start gap-3 md:items-center",
-        className,
-      )}
-    >
-      <Info stroke="var(--color-background-primary)" fill="var(--color-brand)" width={24} height={24} />
+    <div className={clsx("flex flex-row justify-between items-start gap-3", className)}>
+      <Info stroke="var(--color-background-primary)" fill="var(--color-info)" width={24} height={24} />
 
       <div className="flex flex-row flex-wrap flex-1 items-center gap-3">
         <p className="text-subtitle3 text-prose-primary">{title}</p>
@@ -31,8 +26,6 @@ const Notification = (props: Props) => {
           {actions}
         </div>
       </div>
-
-      {onClose && <X className="cursor-pointer mr-2" width={20} height={20} onClick={onClose} />}
     </div>
   );
 };
