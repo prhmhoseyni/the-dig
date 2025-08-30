e550ceaaed6ad66e1b84612c94adf5545821f21e
Parham Hoseyni <150419715+prhmhoseyni@users.noreply.github.com>
2025-08-30 09:02:11 +0330

    feat: update slider styles

--- packages/ui/src/Slider/index.tsx
+++ packages/ui/src/Slider/index.tsx
@@ -1,6 +1,24 @@
+import { cloneElement } from "react";
 import RCSlider, { type SliderProps } from "rc-slider";
-import "rc-slider/assets/index.css";
+import "./index.css";
 
 export default function Slider(props: SliderProps<number | number[]>) {
-  return <RCSlider {...props} />;
+  return (
+    <RCSlider
+      // @ts-expect-error (rc-slider has some typing issues)
+      handleRender={(node, { value, offset }) =>
+        cloneElement(node, {
+          children: (
+            <div
+              className="absolute -mt-10 rounded-lg p-2 shadow-alert bg-gray-700 text-subtitle5 text-prose-inverse whitespace-nowrap"
+              style={{ left: `${offset}%`, transform: "translateX(40%)" }}
+            >
+              {Math.round(Number(value))}%
+            </div>
+          ),
+        })
+      }
+      {...props}
+    />
+  );
 }
