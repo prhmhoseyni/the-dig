9d55d9f172b152a94d25ec224acc678c588795c6
AmirHosseinRP <rezapanahhf1@gmail.com>
2025-09-07 13:29:21 +0330

    feat/segment-control-docs

---
+++ apps/docs/docs/components/SegmentControlWrapper.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import SegmentControl from "@repo/ui/SegmentControl";
+import { useState } from "react";
+
+export default function SegmentControlWrapper() {
+  const [value, setValue] = useState<string>();
+
+  return (
+    <SegmentControl
+      size="sm"
+      value={value}
+      onChange={(value) => setValue(value)}
+    >
+      <SegmentControl.Item value="1">عنوان نمونه 1</SegmentControl.Item>
+      <SegmentControl.Divider />
+      <SegmentControl.Item value="2">عنوان نمونه 2</SegmentControl.Item>
+      <SegmentControl.Divider />
+      <SegmentControl.Item value="3">عنوان نمونه 3</SegmentControl.Item>
+      <SegmentControl.Divider />
+      <SegmentControl.Item value="4">عنوان نمونه 4</SegmentControl.Item>
+    </SegmentControl>
+  );
+}
