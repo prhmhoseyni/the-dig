201f746de965099e11399bda156f32b806d73d61
Parham Hoseyni <150419715+prhmhoseyni@users.noreply.github.com>
2025-09-15 11:27:42 +0330

    fix: fix import tailwindcss

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -63,7 +63,7 @@ const config: Config = {
 					onUntruncatedBlogPosts: "warn",
 				},
 				theme: {
-					customCss: "./src/css/custom.css",
+					customCss: ["./src/css/custom.css", "@repo/ui/styles"],
 				},
 			} satisfies Preset.Options,
 		],
