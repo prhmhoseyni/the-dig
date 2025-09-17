ccc50d59c78b3a308d9dc0be7eecaf5ac6dd66cd
Parham Hoseyni <150419715+prhmhoseyni@users.noreply.github.com>
2025-09-17 16:40:41 +0330

    fix: fix doc

---
+++ apps/docs/src/components/RedirectLink/index.tsx
@@ -0,0 +1,16 @@
+import { ExternalLink } from "lucide-react";
+import type { PropsWithChildren } from "react";
+
+export default function RedirectLink(props: PropsWithChildren<{ href: string }>) {
+	return (
+		<a
+			href={props.href}
+			target="_blank"
+			rel="noopener"
+			className="flex items-center gap-1.5 text-prose-link text-label1"
+		>
+			{props.children}
+			<ExternalLink size={15} />
+		</a>
+	);
+}
