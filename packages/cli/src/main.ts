44bd6bafdec475dc12493fa00984ba09f4a5c78d
Parham Hoseyni <150419715+prhmhoseyni@users.noreply.github.com>
2025-09-22 12:56:32 +0330

    feat: add update cli

--- packages/cli/src/main.ts
+++ packages/cli/src/main.ts
@@ -4,6 +4,7 @@ import { Command } from "commander";
 import { add } from "./commands/add";
 import { config } from "./commands/config";
 import { init } from "./commands/init";
+import { update } from "./commands/update";
 
 const program = new Command();
 
@@ -14,5 +15,11 @@ program.command("init").description("Initialize the project for the-dig").action
 program.command("config").description("Configure the project for the-dig").action(config);
 
 program.command("add").description("Add components").argument("<components-name>", "Name of the components to add").action(add);
+
+program
+	.command("update")
+	.description("Update components")
+	.argument("<components-name>", "Name of the components to update")
+	.action(update);
 
 program.parse();
