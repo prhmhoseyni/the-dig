c231e43f4ebe81863d88962ab882db922c7fbab7
mohammad sharifizade <m.sharifizade@fanap.ir>
2025-10-19 10:59:22 +0330

    change : add disable days and disable range days in datepicker

--- apps/docs/src/components/DatePickerDisableRangeDays/index.tsx
+++ apps/docs/src/components/DatePickerDisableRangeDays/index.tsx
@@ -2,18 +2,19 @@ import DatePicker from "@repo/ui/DatePicker";
 import { useState } from "react";
 import Preview from "../Preview";
 
-export default function DatePickerPreview(props: { local: "fa" | "en" }) {
+export default function DatePickerDisableRangeDays() {
 	const [value, setValue] = useState<number | null>(null);
 
 	return (
 		<Preview>
 			<DatePicker
-				locale={props.local}
+				locale="fa"
 				value={value}
-				onChange={(value) => {
-					console.log("val date : ", value);
-					setValue(value);
+				disabledRange={{
+					start: { year: 1404, month: 7, day: 10 },
+					end: { year: 1404, month: 8, day: 25 },
 				}}
+				onChange={(value) => setValue(value)}
 			/>
 		</Preview>
 	);
