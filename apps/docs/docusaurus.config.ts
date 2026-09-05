095682f0508307e7bcb48bc446d30e07d876976c
PRHM <sj.seyedhoseyni@fanap.ir>
2026-09-05 12:21:46 +0330

    fix: fix build errors

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -9,11 +9,6 @@ const config: Config = {
   tagline: "Different Experience in Design",
   favicon: "img/favicon.ico",
 
-  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
-  future: {
-    v4: true, // Improve compatibility with the upcoming Docusaurus v4
-  },
-
   // Set the production url of your site here
   url: "https://your-docusaurus-site.example.com",
   // Set the /<baseUrl>/ pathname under which your site is served
@@ -26,7 +21,11 @@ const config: Config = {
   projectName: "docusaurus", // Usually your repo name.
 
   onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: "warn",
+    },
+  },
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
